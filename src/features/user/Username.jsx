9ce9652df7cccd12a134cb54738031029e3e1ae0
bikokwa <34458781+bikokwa@users.noreply.github.com>
2024-05-20 18:48:40 +0300

    flexbox

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold hidden">Bikokwa</div>
+  return <div className="text-sm font-semibold hidden md:block">Bikokwa</div>
 }
 
 export default Username
